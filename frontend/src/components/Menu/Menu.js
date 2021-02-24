fc5b27d470f697a111bdb764835e29698519a7a4
Piaero <allyft@gmail.com>
2021-02-24 16:33:33 +0100

    Update Menu.js

--- frontend/src/components/Menu/Menu.js
+++ frontend/src/components/Menu/Menu.js
@@ -5,14 +5,14 @@ export class Menu extends React.Component {
   constructor() {
     super();
     this.state = {
-      menuList: ['Newsy', 'Ogłoszenia ligowe', 'Wyścigi', 'Klasyfikacje', 'Kalendarz', 'Regulamin', 'Zapisy', 'Sezony Archiwalne'],
+      menuList: ['Newsy', 'Ogłoszenia ligowe', 'Klasyfikacje', 'Kalendarz', 'Regulamin', 'Zapisy', 'Sezony Archiwalne'],
       wyścigi: {
         races: ['Australia', 'Bahrajn', 'Chiny', 'Azerbejdżan'],
         options: ['Wyniki', 'Sędziowska', 'Dyskusje', 'Driver Of The Day', 'Transmisje', 'Powtórki']
       },
       sezony: {
         seasons: ['Sezon 1 F1 2018', 'Sezon 2 F1 2019', 'Sezon 3 F1 2019'],
-        options: ['Klasyfikacje', 'Nitki wyścigowe', 'Kalendarz']
+        options: ['Klasyfikacje', 'Kalendarz']
       }
     };
   }
@@ -23,24 +23,7 @@ export class Menu extends React.Component {
         <ul class="dropdown">
           <li><a href="#">Newsy</a></li>
           <li><a href="#">Ogłoszenia ligowe</a></li>
-          <li><a href="#">Wyścigi</a>
-            <ul>
-              {this.state.wyścigi.races.map((race) => {
-                return (
-                  <li><a href="">{race}</a>
-                    <ul>
-                      {this.state.wyścigi.options.map((option) => {
-                        return (
-                          <li><a href="">{option}</a></li>
-                        )
-                      })}
-                    </ul>
-                  </li>
-                )
-              })}
-            </ul>
-          </li>
-
+          <li><a href="#">Wyścigi</a></li>
           <li><a href="#">Klasyfikacje</a>
             <ul>
               <li><a href="">Tabela kierowców</a></li>
@@ -68,6 +51,7 @@ export class Menu extends React.Component {
               })}
             </ul>
           </li>
+          <li><a href="#">Mój profil</a></li>
         </ul>
       </nav>
     )
