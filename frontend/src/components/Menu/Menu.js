81cbfd56232c78b11214d16d0e4afde792e011e5
Piaero <allyft@gmail.com>
2021-02-24 16:41:02 +0100

    Update Menu.js

--- frontend/src/components/Menu/Menu.js
+++ frontend/src/components/Menu/Menu.js
@@ -5,11 +5,6 @@ export class Menu extends React.Component {
   constructor() {
     super();
     this.state = {
-      menuList: ['Newsy', 'Ogłoszenia ligowe', 'Klasyfikacje', 'Kalendarz', 'Regulamin', 'Zapisy', 'Sezony Archiwalne'],
-      wyścigi: {
-        races: ['Australia', 'Bahrajn', 'Chiny', 'Azerbejdżan'],
-        options: ['Wyniki', 'Sędziowska', 'Dyskusje', 'Driver Of The Day', 'Transmisje', 'Powtórki']
-      },
       sezony: {
         seasons: ['Sezon 1 F1 2018', 'Sezon 2 F1 2019', 'Sezon 3 F1 2019'],
         options: ['Klasyfikacje', 'Kalendarz']
@@ -24,13 +19,7 @@ export class Menu extends React.Component {
           <li><a href="#">Newsy</a></li>
           <li><a href="#">Ogłoszenia ligowe</a></li>
           <li><a href="#">Wyścigi</a></li>
-          <li><a href="#">Klasyfikacje</a>
-            <ul>
-              <li><a href="">Tabela kierowców</a></li>
-              <li><a href="">Tabela konstruktorów</a></li>
-              <li><a href="">Punkty Karne</a></li>
-            </ul>
-          </li>
+          <li><a href="#">Klasyfikacje</a></li>
           <li><a href="#">Kalendarz</a></li>
           <li><a href="#">Regulamin</a></li>
           <li><a href="#">Zapisy</a></li>
