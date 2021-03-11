f6e33bedf27844f84937e65dc63cba3d1111e9fb
Piaero <allyft@gmail.com>
2021-03-11 15:32:15 +0100

    Move /divisions API call to seperate file.

--- backend/server.js
+++ backend/server.js
@@ -4,38 +4,15 @@ const path = require("path");
 const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
 
+const divisions = require("./Controllers/divisions.js");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// MongoDB Database
-const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-client.connect((err) => {
-  if (err) throw err;
-  const db = client.db("RGL");
-  const countersCollection = db.collection("divisions");
-  console.log("connected to Database");
-});
-
-app.get("/divisions", (req, res) => {
-  client
-    .db("RGL")
-    .collection("divisions")
-    .find()
-    .project({ division: 1, path: 1, gameLogo: 1 })
-    .toArray()
-    .then((results) => {
-      res.json(results);
-    })
-    .catch((error) => console.error(error));
-});
+app.get("/divisions", divisions);
 
 // Heroku deployment compatibility:
 if (process.env.NODE_ENV === "production") {
