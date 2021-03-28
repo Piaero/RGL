b14f8f42e7341dcd2757db55db5acbbabe5c5be0
Piaero <allyft@gmail.com>
2021-03-28 20:34:39 +0200

    Remove unnecessary comment.

--- frontend/src/wrappers/formatDate.js
+++ frontend/src/wrappers/formatDate.js
@@ -5,7 +5,6 @@ export const formatDate = (fullDate) => {
     today.getDate() + '.' + (today.getMonth() + 1) + '.' + today.getFullYear();
 
   var time = today.getHours() + ':' + today.getMinutes();
-  //  + ':' + today.getSeconds();
 
   var dateFormatted = date + ', ' + time;
 
