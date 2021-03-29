e3ad9ce6fedd7402a7b859efe55162507b39acab
Piaero <allyft@gmail.com>
2021-03-29 23:57:46 +0200

    Remove unnecessary dependency.

--- frontend/src/components/Pages/Calendar/Calendar.js
+++ frontend/src/components/Pages/Calendar/Calendar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouteMatch } from 'react-router-dom';
 
 export const Calendar = () => {
