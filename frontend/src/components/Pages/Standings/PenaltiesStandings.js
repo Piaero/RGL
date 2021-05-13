f869ccabf7b868b4c0753b694c11b1f34e4b6bbe
Piaero <allyft@gmail.com>
2021-05-13 19:16:10 +0200

    Don't show drivers that don't have penalties.

--- frontend/src/components/Pages/Standings/PenaltiesStandings.js
+++ frontend/src/components/Pages/Standings/PenaltiesStandings.js
@@ -16,23 +16,27 @@ export const PenaltiesStandings = ({ penaltiesStandings }) => {
         </thead>
         <tbody>
           {penaltiesStandings.map((driver, index) => {
-            return (
-              <tr key={index}>
-                <td className='results__position'>{index + 1}</td>
-                <td
-                  className='results__number'
-                  style={{
-                    color: driver.teamColour,
-                    textShadow: `0px 0px 4px ${driver.teamColour}, 1px 0 black, 0 1px black, -1px 0 black, 0 -1px black`,
-                  }}
-                >
-                  {driver.number}
-                </td>
-                <td className='results__full-name'>{driver.fullName}</td>
-                <td className='results__nick'>{driver.nick}</td>
-                <td className='results__points'>{driver.penalties}</td>
-              </tr>
-            );
+            if (!driver.penalties) {
+              return null;
+            } else {
+              return (
+                <tr key={index}>
+                  <td className='results__position'>{index + 1}</td>
+                  <td
+                    className='results__number'
+                    style={{
+                      color: driver.teamColour,
+                      textShadow: `0px 0px 4px ${driver.teamColour}, 1px 0 black, 0 1px black, -1px 0 black, 0 -1px black`,
+                    }}
+                  >
+                    {driver.number}
+                  </td>
+                  <td className='results__full-name'>{driver.fullName}</td>
+                  <td className='results__nick'>{driver.nick}</td>
+                  <td className='results__points'>{driver.penalties}</td>
+                </tr>
+              );
+            }
           })}
         </tbody>
       </table>
